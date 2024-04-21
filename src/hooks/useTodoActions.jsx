9eefa6d07549a0c336541413eec0aba7997abb79
URIAN VIERA <urian1213viera@gmail.com>
2024-04-21 14:22:56 -0500

    mejorando el code

--- src/hooks/useTodoActions.jsx
+++ src/hooks/useTodoActions.jsx
@@ -1,33 +1,28 @@
 import { useState } from "react";
 import { toast } from "react-toastify";
-
-import {
-  useTodoReducer,
-  ADD_TASK,
-  TOGGLE_TASK_COMPLETION,
-  DELETE_TASK,
-} from "../reducers/todoReducer";
+import { useTodoReducer } from "../reducers/todoReducer"; // Importa solo el hook de useTodoReducer
+import { TYPES } from "../actions/Types"; // Importa las constantes de Types.js
 
 export const useTodoActions = () => {
   const [tasks, dispatch] = useTodoReducer();
   const [toastId, setToastId] = useState(null);
 
   const addTask = (text, completed) => {
-    dispatch({ type: ADD_TASK, payload: { text, completed } });
+    dispatch({ type: TYPES.CREAR_TAREA, payload: { text, completed } });
     if (!toast.isActive(toastId)) {
       setToastId(toast.success("Tarea agregada correctamente", { toastId }));
     }
   };
 
   const toggleTaskCompletion = (id) => {
-    dispatch({ type: TOGGLE_TASK_COMPLETION, payload: { id } });
+    dispatch({ type: TYPES.TOGGLE_TASK, payload: { id } });
     if (!toast.isActive(toastId)) {
       setToastId(toast.success("Tarea completada correctamente", { toastId }));
     }
   };
 
   const deleteTask = (id) => {
-    dispatch({ type: DELETE_TASK, payload: { id } });
+    dispatch({ type: TYPES.DELETE_TASK, payload: { id } });
     if (!toast.isActive(toastId)) {
       setToastId(toast.error("Tarea eliminada correctamente", { toastId }));
     }
--- src/reducers/todoReducer.jsx
+++ src/reducers/todoReducer.jsx
@@ -1,24 +1,18 @@
 import { useReducer } from "react";
-
-//import { TYPES } from "../actions/Types";
-
-// Definimos los tipos de acciones que pueden ocurrir en nuestra lista de tareas
-export const ADD_TASK = "ADD_TASK";
-export const TOGGLE_TASK_COMPLETION = "TOGGLE_TASK_COMPLETION";
-export const DELETE_TASK = "DELETE_TASK";
+import { TYPES } from "../actions/Types";
 
 // Reducer para manejar las acciones en la lista de tareas
 export const todoReducer = (state, action) => {
-  if (action.type === "ADD_TASK") {
+  if (action.type === TYPES.CREAR_TAREA) {
     return [
       ...state,
       { id: state.length + 1, text: action.payload.text, completed: action.payload.completed },
     ];
-  } else if (action.type === "TOGGLE_TASK_COMPLETION") {
+  } else if (action.type === TYPES.TOGGLE_TASK) {
     return state.map((task) =>
       task.id === action.payload.id ? { ...task, completed: !task.completed } : task
     );
-  } else if (action.type === "DELETE_TASK") {
+  } else if (action.type === TYPES.DELETE_TASK) {
     return state.filter((task) => task.id !== action.payload.id);
   } else {
     return state;
